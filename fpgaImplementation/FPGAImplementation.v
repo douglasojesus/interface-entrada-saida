fa73a971d7d569c05674aa63e653d52b9a2e9bbc
douglas de jesus <dougecomp@gmail.com>
2023-08-24 21:41:49 -0300

    :sparkles: feat: Adicionando código do decodificador para teste.

--- fpgaImplementation/FPGAImplementation.v
+++ fpgaImplementation/FPGAImplementation.v
@@ -1,16 +1,20 @@
-module FPGAImplementation(i_Clock, i_Rx_Serial, o_Rx_DV, o_Rx_Byte, i_Tx_DV, i_Tx_Byte, o_Tx_Active, o_Tx_Serial, o_Tx_Done);
+module FPGAImplementation(clock, bitSerialAtual, bitsEstaoRecebidos, byteCompleto, i_Tx_DV, i_Tx_Byte, o_Tx_Active, o_Tx_Serial, o_Tx_Done, display);
 
-	input 			i_Clock;
-	input 			i_Rx_Serial;
-	output 			o_Rx_DV;
-	output [7:0] 	o_Rx_Byte;
+	input 			clock;
+	input 			bitSerialAtual;
+	output 			bitsEstaoRecebidos;
+	output [7:0] 	byteCompleto;
 	input 			i_Tx_DV;
 	input [7:0] 	i_Tx_Byte; 
 	output 			o_Tx_Active;
 	output 			o_Tx_Serial;
 	output 			o_Tx_Done;
+	output [6:0]	display;
+	
 
-	uart_rx (i_Clock, i_Rx_Serial, o_Rx_DV, o_Rx_Byte);
-	uart_tx (i_Clock, i_Tx_DV, i_Tx_Byte, o_Tx_Active, o_Tx_Serial, o_Tx_Done);
+	uart_rx (clock, bitSerialAtual, bitsEstaoRecebidos, byteCompleto);
+	uart_tx (clock, i_Tx_DV, i_Tx_Byte, o_Tx_Active, o_Tx_Serial, o_Tx_Done);
+	
+	decoder (byteCompleto, display);
 
 endmodule
